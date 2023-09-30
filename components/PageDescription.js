380e3347d1d16e38a279486a8780456b28e2ab3c
bonheurbaleks <bonheurbalekoua@gmail.com>
2023-09-30 21:44:30 +0300

    added PageDescription component

---
+++ components/PageDescription.js
@@ -0,0 +1,16 @@
+export default function PageDescription() {
+  return (
+    <p className="pt-10 mr-20 ml-20 pl-5">
+      ROCHELHAIR propose la vente en gros et au particulier d’extensions de
+      cheveux 100% naturels, de perruques haut de gamme et d’autres accessoires
+      et compléments capillaires. ROCHELHAIR offre à ses clients de cheveux 100%
+      NATURELS soigneusement sélectionnés, et une large gamme de couleurs pour
+      vos tissages, chignons, perruques, et prothèses capillaires. Les
+      extensions de cheveux naturels ont longtemps été considérées comme un
+      luxe, peu accessible et réservé aux icônes de la mode ou du
+      divertissement. En appliquant des tarifs plus que raisonnables, ROCHELHAIR
+      met à la portée de tous des cheveux naturels d’excellente qualité, d’une
+      grande souplesse, d’une brillance et d’une beauté inégalée.
+    </p>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 
 import HeaderBottom from "@/components/HeaderBottom";
 import Header from "@/components/Header";
+import PageDescription from "@/components/PageDescription";
 
 export default function Home() {
   return (
     <>
       <Header />
       <HeaderBottom />
+      <PageDescription />
     </>
   );
 }
