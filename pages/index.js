0e9c603c1f0e4ef1af750158c7a67b8120a8255e
bonheurbaleks <bonheurbalekoua@gmail.com>
2023-10-13 10:55:35 +0300

    updated empty files

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+import HeaderBottom from "../components/HeaderBottom";
+import Header from "../components/Header";
+import PageDescription from "../components/PageDescription";
+import Footer from "../components/Footer";
+import ImagenIcons from "../components/ImagenIcons";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <HeaderBottom />
+      <ImagenIcons />
+      <PageDescription />
+      <Footer />
+    </>
+  );
+}
